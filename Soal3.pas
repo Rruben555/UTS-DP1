68679a7ee742b446d7f36ef8367250dfa9f7dcdf
Rruben555 <rubensiregar25@gmail.com>
2024-10-24 20:58:22 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,24 @@
-//ketik kode jawaban noo 3 kamu disini
+uses crt;
+var
+n,p,q:integer;//deklarasi variabel integer
+begin//mulai
+    clrscr;//clearscreen
+    write('n='); readln(n);//input jumlah n, mis:5
+    if n mod 2 = 0 then//jika genap
+        write('angka tidak valid, harus angka ganjil')//tulis
+    else
+        for p:=1 to n do//fungsi vertikal n-kali 
+        if p<=n then
+        begin
+            repeat//perulangan fungsi horizontal
+            for q:=1 to n do
+                if q=p then//gambar garis (\)
+                    write('*')
+                else if q=n-p+1 then//gambar gsris (/)
+                    write('*')
+                else //kosongkan yang lain
+                    write(' ');
+            until q=n;//sampai n=kali
+            writeln;//pemisah baris
+        end;
+end.//selesai
