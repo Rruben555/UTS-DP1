6131a5461f9fec58eb879ed1d19d2e8c5f83050d
Rruben555 <rubensiregar25@gmail.com>
2024-10-31 13:23:15 +0700

    Update Soal 4.pas

--- Soal 4.pas
+++ Soal 4.pas
@@ -1,83 +1,36 @@
 uses crt;
 var
-i:integer;//deklarasi variabel integer
+i: integer;//deklarasi variabel integer
 sisa: longint;//deklarasi variabel longint
 begin//mulai
-    clrscr;//clearscreen
-    readln(sisa);//input nilai sisa, mis:62500
-    i:=0;//pemberian nilai i
+clrscr;//clearscreen
+readln(sisa);//input nilai kembalian 
+i:=0;//pemberian nilai i
+while sisa>100 do//perulangan ketika sisa>100
     begin
-    repeat//pengulangan
-    if sisa>=100000 then//jka sisa>=100000
-        begin
-        sisa:=sisa-100000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end
-    else if sisa<100000 then//jika sisa dibawah 100000
-        break;//keluar dari loop 
-    until sisa<100000;//sampai sisa<100000
-    if sisa>=75000 then//jika sisa>=75000
-        begin
-        sisa:=sisa-75000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
-    if sisa>=50000 then//jika sisa>=50000
-        begin
-        sisa:=sisa-50000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
-    repeat//pengulangan
-    if sisa>=20000 then//jika sisa>=20000
-        begin
-        sisa:=sisa-20000;//update nilai 20000
-        i:=i+1;//update nilai i
-        end
-    else if sisa=0 then//jka sisa habis
-        break;//keluar dari loop
-    until sisa<20000;//sampai sisa<20000
-    if sisa>=10000 then//jika sisa>=10000
-        begin
-        sisa:=sisa-10000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
-    if sisa>=5000 then//jika sisa>=5000
-        begin
-        sisa:=sisa-5000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
-    repeat//pengulangan
-    if sisa>=2000 then//jika sisa>2000
-        begin
-        sisa:=sisa-2000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end
-    else if sisa<2000 then//jika sisa<2000
-        break;//keluar dari loop
-    until sisa<2000;//sampai sisa<2000
-    if sisa>=1000 then//jika sisa>=1000
-        begin
-        sisa:=sisa-1000;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
-    if sisa>=500 then//jika sisa>=500
-        begin
-        sisa:=sisa-500;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
-    repeat//pengulangan
-    if sisa>=200 then//jika sisa >=200
-        begin
-        sisa:=sisa-200;//update nilai s
-        i:=i+1;//update nilai i
-        end
-    else if sisa<200 then//jika sisa<200
-        break;//keluar dari loop
-    until sisa<200;//sampai sisa<200
-    if sisa>=100 then//jika sisa>=100
-        begin
-        sisa:=sisa-100;//update nilai sisa
-        i:=i+1;//update nilai i
-        end;
+    if sisa>=100000 then//jika sisa>=100000
+    sisa:=sisa-100000//update nilai sisa (dikurang 100000)
+    else if sisa>=75000 then//jika sisa>=75000
+    sisa:=sisa-75000//update nilai sisa (dikurang 75000)
+    else if sisa>=50000 then//jika sisa>=50000
+    sisa:=sisa-50000//update nilai sisa (dikurang 50000)
+    else if sisa>=20000 then//jika sisa>=20000
+    sisa:=sisa-20000//update nilai sisa (dikurang 20000)
+    else if sisa>=10000 then//jika sisa>=10000
+    sisa:=sisa-10000//update nilai sisa (dikurang 10000)
+    else if sisa>=5000 then//jika sisa>=5000
+    sisa:=sisa-5000//update nilai sisa (dikurang 5000)
+    else if sisa>=2000 then//jika sisa>=2000
+    sisa:=sisa-2000//update nilai sisa (dikurang 2000)
+    else if sisa>=1000 then//jika sisa>=1000
+    sisa:=sisa-1000//update nilai sisa (dikurang 1000)
+    else if sisa>=500 then//jika sisa>=500
+    sisa:=sisa-500//update nilai sisa (dikurang 500)
+    else if sisa>=200 then//jika sisa>=200
+    sisa:=sisa-200//update nilai sisa (dikurang 200)
+    else if sisa>=100 then//jika sisa>=100
+    sisa:=sisa-100;//update nilai sisa (dikurang 100)
+    i:=i+1;//update nilai i
     end;
-    write(i);//output nilai i
-end.//selesai
+writeln(i);//tampilkan i
+end.
